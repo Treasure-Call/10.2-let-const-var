bbfaf0c7584bf7a5e196abef1da893ba80cbfcd3
Treasure-Call <86845782+Treasure-Call@users.noreply.github.com>
2022-02-14 20:02:33 -0800

    Commit let-const-var Exercise

---
+++ let-const-var.js
@@ -0,0 +1,27 @@
+// ES5 Global Constants
+var PI = 3.14;
+PI = 42;
+
+// ES2015 Global Constants
+
+const PI = 3.14;
+PI = 42;
+
+
+/* 
+
+Quiz
+
+What is the difference between var and let?
+    -var and let can both be reassigned and mutated. However, unlike var, let cannot be redeclated. In addition, let uses block scope while var uses function scope.
+
+What is the difference between var and const?
+    -while var can be reassigned and redeclared, const cannot. In addition, const uses block scope while var uses function scope.
+
+What is the difference between let and const?
+    -let can be reassigned while const cannot. Otherwise, they are similar.
+
+What is hoisting?
+    -hoisting is where the compiler appears to move the var variable declaration to the top of the page. Variables can also be lifted to the top of the scopes that they are in.
+
+*/
